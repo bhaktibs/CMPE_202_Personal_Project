8754da908146357b52b6952b8c891646cd86f7f6
bhaktibs <bhakti.shah@sjsu.edu>
2017-04-21 22:32:43 -0700

    Update Class_type.java

--- src/main/java/Class_type.java
+++ src/main/java/Class_type.java
@@ -19,10 +19,9 @@
  * GNU Lesser General Public License for more details.
  */
 
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.BodyDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 
 import java.util.ArrayList;
@@ -40,9 +39,10 @@ public class Class_type {
     private List<MethodDeclaration> methods;
     private ArrayList<String> method_ext;
     private NodeList<BodyDeclaration<?>>  constructor;
-    private String cu_list_string;
+    private ArrayList<String> cu_list_string;
+    private String depedency_relations="";
 
-    public Class_type(ClassOrInterfaceDeclaration c,String cu_list) {
+    public Class_type(ClassOrInterfaceDeclaration c,ArrayList<String> cu_list) {
         name = c.getNameAsString();
         parent = c.getExtendedTypes();
         implemented_type = c.getImplementedTypes();
@@ -52,19 +52,24 @@ public class Class_type {
         constructor = c.getMembers();
         cu_list_string=cu_list;
 
+
         //method extractor converts method to final string form which can be used directly to create class
         for (MethodDeclaration m : methods) {
-
-            Method_extractor m_e = new Method_extractor(m,cu_list_string);
+            Method_extractor m_e = new Method_extractor(m);
             method_ext.add(m_e.get_string());
 
         }
     }
-public String get_name()
+public String get_dependency_relations()
 {
-    return "class "+name;
+    return this.depedency_relations;
 }
-public String get_relations() {
+    //returns relations to other class or interface
+    public String get_name() {
+        return "class " + name;
+    }
+
+    public String get_relations() {
         String relations = "";
         for (ClassOrInterfaceType p : parent) {
             relations += p.getNameAsString();
@@ -74,64 +79,103 @@ public String get_relations() {
             relations += i.getNameAsString();
             relations += "<|.." + name + "\n";
         }
+
         return relations;
     }
-public String get_method_members()
-    {
-        final_string +="class "+name+"{\n";
+public String get_constructor_string(BodyDeclaration b)
+{
+    String constructor_to_string="";
 
-        if(member_variable!=null)
-        {
-            for(int i=0;i<member_variable.size();i++)
-            {
-                final_string +=member_variable.get(i) + "\n";
+
+              if (b instanceof ConstructorDeclaration)
+              {
+                  constructor_to_string += "+ " + ((ConstructorDeclaration) b).getName().toString() +" (";
+                  for (Parameter p:((ConstructorDeclaration) b).getParameters()
+                       ) {
+                      for(int i=0;i<cu_list_string.size();i++)
+                      {
+                          if(p.getType().toString().contains(cu_list_string.get(i)))
+                          {
+                              depedency_relations += this.name + "..>" + cu_list_string.get(i) + "\n";
+                          }
+                      }
+                      constructor_to_string += p.getNameAsString()+": "+ p.getType().toString();
+                  }
+                  constructor_to_string +=")";
+              }
+    //System.out.println("this is constructor " + constructor_to_string);
+              return constructor_to_string;
+
+}
+    // return all the methods and attributes of the class.
+    public String get_method_members() {
+        final_string += "class " + name + "{\n";
+        if(constructor!= null) {
+            for (BodyDeclaration b : constructor) {
+                for(int i=0;i<cu_list_string.size();i++)
+                    if(b instanceof MethodDeclaration ) {
+                        if (((MethodDeclaration) b).getDeclarationAsString().contains(cu_list_string.get(i)))
+                        {
+                            if(depedency_relations.contains(this.name + "..>" + cu_list_string.get(i)))
+                            {
+
+                            }
+                            else {
+                                depedency_relations += this.name + "..>" + cu_list_string.get(i) + "\n";
+                            }
+//                    System.out.println(depedency_relations);
+// System.out.println(((MethodDeclaration) b).getDeclarationAsString() + "name " + this.name + "true" +
+// cu_list_string.get(i));
+                        }
+                    }
+                final_string +=get_constructor_string(b)+"\n";
             }
         }
 
-        if(method_ext!=null) {
+
+        if (attributes != null) {
+            for (int i = 0; i < attributes.size(); i++) {
+
+                final_string += to_string_attribute(attributes.get(i)) + "\n";
+                for(int j=0;j<cu_list_string.size();j++)
+                {
+                    if (attributes.get(i).getElementType().toString().contains(cu_list_string.get(j)))
+                    {
+                        depedency_relations+=this.name + "--" + cu_list_string.get(j) + "\n";
+                    }
+                }
+                //System.out.println("this are memeber variables of class "+ member_variable.get(i));
+            }
+        }
+
+        if (method_ext != null) {
             for (int i = 0; i < method_ext.size(); i++) {
+               // System.out.println("this is methods of class" + method_ext.get(i));
                 final_string += method_ext.get(i) + "\n";
             }
         }
-        final_string+="}";
-        //System.out.println(final_string);
+        final_string += "}";
+        //System.out.println("this is for class "+final_string);
         return final_string;
     }
-public String convert_to_att_form(BodyDeclaration<?> a)
-   {
-       a.removeComment();
-       String attribute = a.toString();
-       attribute = attribute.replace(";","");
 
-       String final_att="";
-       if(attribute.contains("private "))
+    public String to_string_attribute(FieldDeclaration a) {
+        String final_att = "";
+       for(Modifier e:a.getModifiers())
        {
-           String result[] = attribute.split("\\s+");
-           final_att += "- ";
-           int index=result.toString().indexOf("private");
-           for(int i=index+1;i<result.length;i++)
+           if(e.toString()=="PRIVATE")
            {
-               final_att += result[i]+" ";
+            final_att += "- " +a.getVariables().get(0).getNameAsString() +": "+a.getVariables().get(0).getType()
+                    .toString();
            }
-
-       }
-       else if(attribute.contains("public"))
-       {
-           String result[] = attribute.split("\\s+");
-           final_att +="+ ";
-           int index=result.toString().indexOf("private");
-           for(int i=index+1;i<result.length;i++)
+           else if(e.toString()=="PUBLIC")
            {
-               final_att += result[i];
+               final_att += "+ " +a.getVariables().get(0).getNameAsString() +": "+a.getVariables().get(0).getType()
+                       .toString();
            }
        }
-      return final_att;
-
-   }
-
+        return final_att;
 
     }
-
-
-
+}
 
