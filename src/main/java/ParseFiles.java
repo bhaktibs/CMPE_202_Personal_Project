f3cbcfccfd79908578a4aa28a1a3450828577106
bhaktibs <bhakti.shah@sjsu.edu>
2017-04-21 22:34:09 -0700

    Update ParseFiles.java

--- src/main/java/ParseFiles.java
+++ src/main/java/ParseFiles.java
@@ -1,39 +1,64 @@
 /*
- * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
+ * Copyright (C) 2011, 2013-2016 The JavaParser Team.
+ *
+ * This file is part of JavaParser.
+ *
+ * JavaParser can be used either under the terms of
+ * a) the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * b) the terms of the Apache License
+ *
+ * You should have received a copy of both licenses in LICENCE.LGPL and
+ * LICENCE.APACHE. Please refer to those files for details.
+ *
+ * JavaParser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  */
 
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
+import net.sourceforge.plantuml.SourceStringReader;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.EnumSet;
 
-;
 
 /**
  * Created by bhaktishah on 2/25/17.
  */
-/*parseFiles has the constructor which does the intial work of parsing the files and creating compilation units for each java
-class. Once compilation units are created, each compilation unit is processed to extract information*/
 public class ParseFiles {
     ArrayList<CompilationUnit> cu_list;
     ArrayList<String> classes;
     private String input_file_path;
     private String output_file;
+    private String name_class_interface;
+    private String relations_class_interface;
+    private String methods_members;
+    private String parser_file;
+    private String dependency_relation="";
+
+
 
     ParseFiles(String in_path, String out_path) {
-        this.input_file_path = in_path;
-        this.output_file = out_path;
+        input_file_path = in_path;
+        output_file = out_path;
+        classes = new ArrayList<String>();
+        name_class_interface = "";
+        relations_class_interface = "";
+        methods_members = "";
+        parser_file = "";
     }
+
 
     public ArrayList<CompilationUnit> get_cu_list() throws Exception {
         File input_folder = new File(input_file_path);
@@ -46,12 +71,15 @@ public class ParseFiles {
                 try {
                     CompilationUnit cu;
                     cu = JavaParser.parse(in);
+                    // System.out.println(cu.toString());
                     cu_arr_list.add(cu);
                 } finally {
                     in.close();
+
                 }
             }
         }
+        //System.out.println(cu_arr_list);
         return cu_arr_list;
 
     }
@@ -60,31 +88,65 @@ public class ParseFiles {
     public void run() throws Exception {
         try {
             cu_list = get_cu_list();
-            for(CompilationUnit c:cu_list) {
-                //System.out.println(c.toString());
+            ArrayList<String> name_of_class_interface = get_names(cu_list);
+            ArrayList<String> dp= new ArrayList<String>();
+           //System.out.println("this is the list of file"+list_of_class);
+            for (CompilationUnit c : cu_list) {
+
                 TypeDeclaration t = c.getType(0);
-                if(t instanceof ClassOrInterfaceDeclaration)
-                {
+                if (t instanceof ClassOrInterfaceDeclaration) {
                     ClassOrInterfaceDeclaration classDec = (ClassOrInterfaceDeclaration) t;
-                    EnumSet<Modifier> e = classDec.getModifiers();
-                    if (e.contains(Modifier.PUBLIC)) {
-                        //The class or interface is public
-                        if (classDec.isInterface()) {
-                            Interface_type int_t = new Interface_type(classDec);  
-                        }
-                        else
-                        {
-                            Class_type class_t = new Class_type(classDec);
-                        }
+                    if (classDec.isInterface()) {
+                        Interface_type int_t = new Interface_type(classDec);
+                        name_class_interface += int_t.get_name() + "\n";
+                        methods_members += int_t.get_method_members() + "\n";
+
+                    } else {
+                        Class_type class_t = new Class_type(classDec,name_of_class_interface);
+                        name_class_interface += class_t.get_name() + "\n";
+                        relations_class_interface += class_t.get_relations() + "\n";
+                        methods_members += class_t.get_method_members() + "\n";
+                        dp.add(class_t.get_dependency_relations());
+
+
                     }
                 }
             }
+            dependency_relation += remove_duplicate_dependecy(dp);
 
-
+            parser_file += "@startuml\n" + name_class_interface + relations_class_interface +dependency_relation+ methods_members +
+                    "@enduml";
+            //System.out.println(parser_file);
         } finally {
-
+            createUML();
         }
+
     }
+    public ArrayList<String> get_names(ArrayList<CompilationUnit> cu)
+    {
+        ArrayList<String>names = new ArrayList<String>();
+        for(CompilationUnit c : cu)
+        {
+            names.add(c.getType(0).getName().toString());
+        }
+
+
+        return  names;
+    }
+    public String remove_duplicate_dependecy(ArrayList<String>dp)
+    {
+        String dependecy="";
+        return dependecy;
+    }
+
+    public void createUML() throws Exception {
+
+        SourceStringReader r = new SourceStringReader(parser_file);
+        OutputStream png = new FileOutputStream(output_file);
+        r.generateImage(png);
+    }
+
 }
+
 
 
